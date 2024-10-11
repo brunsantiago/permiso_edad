f7717fed147fb479d021e8d1552b65b5f424848b
Santiago Brun <brunsantiago@hotmail.com>
2024-10-11 15:05:49 -0300

    Primer Commit

---
+++ algoritmos3.py
@@ -0,0 +1,8 @@
+# Algoritmo que muestra si puede pasar o no a un boliche segun la edad
+
+edad = int(input("Por favor ingrese su edad: "))
+
+if edad >= 18:
+    print("Usted puede ingresar al boliche")
+else:
+    print("Lo sentimos Uds no puede pasar")
